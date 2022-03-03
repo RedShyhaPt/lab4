ad58093bec710e08428f299dcb0c543cf69ba02f
RedShyhaPt <64642070+RedShyhaPt@users.noreply.github.com>
2022-03-03 12:15:31 +0300

    Add math.cpp& output rezult one of it function row

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <math.cpp>
 
 int main() {
 	std::cout << "Hello world!";
+	salam ();
 	
+	int x = 9;
+	std::cout << row (x,2);
 	std::system ("pause");
 	return 0;
 }
